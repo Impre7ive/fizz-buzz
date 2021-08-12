7743198fb3640d344d4b43b9163e88f0a4b3a12c
Dmitry Liashenko <adslyashenko@gmail.com>
2021-08-12 17:31:59 +0300

    Extra space added.

--- Fizz Buzz/Program.cs
+++ Fizz Buzz/Program.cs
@@ -11,6 +11,7 @@ namespace Fizz_Buzz {
 
             Console.ReadLine();
         }
+
         private void FizzBuzz(byte i) {
             Console.WriteLine((i % 3 == 0 && i % 5 == 0) ? "FizzBuzz" : (i % 3 == 0) ? "Fizz" : (i % 5 == 0) ? "Buzz" : i.ToString());
         }
